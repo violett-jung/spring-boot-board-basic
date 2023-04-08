bc1a70e7cc3ad1c4f9e097abbb15bc44f786d332
violett-jung <violett.jung@gmail.com>
2023-04-08 14:40:10 +0900

    Add board list view functionality
    - Create boardlist.html
    - Add boardList method to BoardService class
    - Add boardList method to BoardController class

--- src/main/java/com/study/board/controller/BoardController.java
+++ src/main/java/com/study/board/controller/BoardController.java
@@ -4,6 +4,7 @@ import com.study.board.entity.Board;
 import com.study.board.service.BoardService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -25,5 +26,13 @@ public class BoardController {
         return "";
     }
 
+    @GetMapping("/board/list")
+    public String boardList(Model model) {
+        model.addAttribute("list", boardService.boardList());
+        return "boardlist";
     }
+
+
+
+}
 
--- src/main/java/com/study/board/service/BoardService.java
+++ src/main/java/com/study/board/service/BoardService.java
@@ -5,6 +5,8 @@ import com.study.board.repository.BoardRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class BoardService {
 
@@ -12,5 +14,10 @@ public class BoardService {
     private BoardRepository boardRepository;
     public void write(Board board){
         boardRepository.save(board);
+    }
+
+    public List<Board> boardList(){
+
+        return boardRepository.findAll();
     }
 }
